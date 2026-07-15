7fd13c873f6f360e90a286bfeb20af45a00cbe1d
Moises Dias <m.dias009@gmail.com>
2026-07-15 12:05:43 -0300

    Updating ConversasFragment and fragment_conversas.xml

--- app/src/main/java/com/moisesdias/fragments/fragments/ConversasFragment.kt
+++ app/src/main/java/com/moisesdias/fragments/fragments/ConversasFragment.kt
@@ -6,10 +6,17 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.moisesdias.fragments.R
 
 class ConversasFragment : Fragment(){
+
+    private lateinit var btnExecutar: Button
+    private lateinit var textNome: TextView
+    private lateinit var editNome: EditText
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -27,11 +34,22 @@ class ConversasFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         Log.i("ciclo_vida", "Fragment onCreateView")
-        return inflater.inflate(
+        val view = inflater.inflate(
             R.layout.fragment_conversas,
             container,
             false
         )
+        btnExecutar = view.findViewById(R.id.btn_executar)
+        editNome = view.findViewById(R.id.edit_nome)
+        textNome = view.findViewById(R.id.text_nome)
+
+        btnExecutar.setOnClickListener {
+            textNome.text = editNome.text.toString()
+        }
+
+
+
+        return view
     }
 
     /*Depreciado
