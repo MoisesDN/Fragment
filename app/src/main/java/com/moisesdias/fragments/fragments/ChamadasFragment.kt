a357e3519cced9bef10f4196b1d4044dcdad2777
Moises Dias <m.dias009@gmail.com>
2026-07-15 10:34:22 -0300

    Implementing dynamic fragment switching

---
+++ app/src/main/java/com/moisesdias/fragments/fragments/ChamadasFragment.kt
@@ -0,0 +1,23 @@
+package com.moisesdias.fragments.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.moisesdias.fragments.R
+
+class ChamadasFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.fragment_chamadas,
+            container,
+            false
+        )
+    }
+}
